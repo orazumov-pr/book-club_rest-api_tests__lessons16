41b8371a0f9b3ac69c8f92e2b0bddf017c84218c
Oleg Razumov <orazumoff@gmail.com>
2026-04-22 12:48:32 +0300

    add Logout test from lesson

--- src/test/java/api/AuthApiClient.java
+++ src/test/java/api/AuthApiClient.java
@@ -1,6 +1,7 @@
 package api;
 
 import models.LoginBodyModel;
+import models.LogoutBodyModel;
 import models.SuccessfulLoginResponseModel;
 import models.WrongCredentialsLoginResponseModel;
 
@@ -8,6 +9,8 @@ import static io.restassured.RestAssured.given;
 import static specs.LoginSpec.loginRequestSpec;
 import static specs.LoginSpec.successfulLoginResponseSpec;
 import static specs.LoginSpec.wrongCredentialsLoginResponseSpec;
+import static specs.LogoutSpec.logoutRequestSpec;
+import static specs.LogoutSpec.successfulLogoutResponseSpec;
 
 
 public class AuthApiClient {
@@ -32,6 +35,26 @@ public class AuthApiClient {
                 .spec(wrongCredentialsLoginResponseSpec)
                 .extract()
                 .as(WrongCredentialsLoginResponseModel.class);
+    }
+
+    public String loginAndGetRefreshToken(LoginBodyModel loginBody) {
+        return given(loginRequestSpec)
+                .body(loginBody)
+                .when()
+                .post("/auth/token/")
+                .then()
+                .spec(successfulLoginResponseSpec)
+                .extract()
+                .path("refresh");
+    }
+
+    public void logout(LogoutBodyModel logoutBody) {
+        given(logoutRequestSpec)
+                .body(logoutBody)
+                .when()
+                .post("/auth/logout/")
+                .then()
+                .spec(successfulLogoutResponseSpec);
     }
 
 }
---
+++ src/test/java/tests/LogoutTests.java
@@ -0,0 +1,20 @@
+package tests;
+
+import models.LoginBodyModel;
+import models.LogoutBodyModel;
+import org.junit.jupiter.api.Test;
+
+import static tests.TestData.LOGIN_PASSWORD;
+import static tests.TestData.LOGIN_USERNAME;
+
+public class LogoutTests extends TestBase {
+
+    @Test
+    public void successfulLogoutTest() {
+        LoginBodyModel loginData = new LoginBodyModel(LOGIN_USERNAME, LOGIN_PASSWORD);
+        String refreshToken = api.auth.loginAndGetRefreshToken(loginData);
+
+        LogoutBodyModel logoutData = new LogoutBodyModel(refreshToken);
+        api.auth.logout(logoutData);
+    }
+}
